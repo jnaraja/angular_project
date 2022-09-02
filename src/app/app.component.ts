3b0a8a1c7b4d44481b2096ce374d75dcd06b7cd2
Jonathan Naraja <j2naraja@gmail.com>
2022-09-02 16:14:18 -0700

    Created onDelete() method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] 
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  firstMediaItem = {
+    id: 1,
+    name: 'Firebug',
+    medium: 'Series',
+    category: 'Science Fiction',
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  };
+}
